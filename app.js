1b06637d7054f630a8b617c744057cc0742747dd
S. Everett Abbott <severettabbott@gmail.com>
2018-01-12 15:06:48 -0800

    attempt 3 at fixing deployment

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
   res.status(404).json({ error: { message: 'Not found' }});
 });
 
-const listener = () => {
+function listener() {
   console.log(`Listening on port ${port}!`);
 }
 
